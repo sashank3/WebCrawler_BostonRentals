6bb7ecb22378130fd367b6f65aabc8662d0aa7bb
Sashank Reddy <sas.reddy123@gmail.com>
2023-08-06 23:59:09 -0400

    Main Code

---
+++ Code/WebpageClass.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  3 20:50:31 2023
+
+@author: sasre
+"""
+'', '', '', '', 
+'', '', ''
+class WebpageData:
+  def __init__(self, last_updated, price, n_beds, n_baths, avbl_date, building_address, area, 
+               agent_name, agent_email, agent_phone,agent_fees, heat, hot_water, electricity, 
+               parking, dish_washer, laundry_location, air_conditioning):
+      
+    self.last_updated = last_updated
+    self.price = price
+    self.n_beds = n_beds
+    self.n_baths = n_baths
+    self.avbl_date = avbl_date
+    self.building_address = building_address
+    self.area = area
+    self.agent_name = agent_name
+    self.agent_email = agent_email
+    self.agent_phone = agent_phone
+    self.agent_fees = agent_fees
+    self.heat = heat
+    self.hot_water = hot_water    
+    self.electricity = electricity
+    self.parking = parking
+    self.dish_washer = dish_washer
+    self.laundry_location = laundry_location
+    self.air_conditioning = air_conditioning
+
+
